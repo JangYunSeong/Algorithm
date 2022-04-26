0060ec2868c04a5d0360afb049171707ccaa8bef
장윤성 <46878756+JangYunSeong@users.noreply.github.com>
2022-04-26 20:46:34 +0900

    1149번:

---
+++ dynamic9.py
@@ -0,0 +1,10 @@
+#1149번
+n = int(input())
+color = []
+for i in range(n):
+    color.append(list(map(int,input().split())))
+for i in range(1,n):
+    color[i][0] = min(color[i-1][1],color[i-1][2]) + color[i][0]
+    color[i][1] = min(color[i-1][0],color[i-1][2]) + color[i][1]
+    color[i][2] = min(color[i-1][0],color[i-1][1]) + color[i][2]
+print(min(color[n-1][0],color[n-1][1],color[n-1][2]))
